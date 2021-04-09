8c3144d7a1eaa61b28ce7dfac674f89c78aafdc4
vinicius <viniciusalmada10@gmail.com>
2021-04-09 01:47:47 -0300

    create program to print in dec, oct and hex

---
+++ 1-Data and C/bases.c
@@ -0,0 +1,11 @@
+/* bases.c -- prints 100 in decimal, octal  and hex */
+#include <stdio.h>
+int main(void) {
+	int x  = 100;
+
+	printf("dec = %d; oct = %o; hex = %x\n", x, x, x);
+
+	printf("dec = %d; oct = %#o; hex = %#x\n", x, x, x);
+
+	return 0;
+}

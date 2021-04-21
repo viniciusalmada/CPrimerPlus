a8711764e71c4bba693a1bbb72bb7c47816e1d23
vinicius <viniciusalmada10@gmail.com>
2021-04-21 17:29:37 -0300

    create code to print chars

---
+++ 1-Data and C/charcode.c
@@ -0,0 +1,19 @@
+/* charcode.c - displays code number for a character */
+#include <stdio.h>
+
+int main() {
+	char ch;
+
+	printf("Please enter a character.\n");
+	scanf("%c", &ch);
+	printf("The code for %c is %d.\n", ch, ch);
+
+	return 0;
+}
+
+/*
+ * Output:
+ * Please enter a character.
+ * C
+ * The code for C is 67.
+ */
